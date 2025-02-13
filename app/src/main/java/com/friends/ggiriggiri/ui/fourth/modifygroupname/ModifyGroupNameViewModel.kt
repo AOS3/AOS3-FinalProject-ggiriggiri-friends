5103cef47f07d57f80cd64d098de2fd2ab53af35
universeYong <boyd0509@gmail.com>
2025-02-14 02:11:25 +0900

    mypage

---
+++ app/src/main/java/com/friends/ggiriggiri/ui/fourth/modifygroupname/ModifyGroupNameViewModel.kt
@@ -0,0 +1,7 @@
+package com.friends.ggiriggiri.ui.fourth.modifygroupname
+
+import androidx.lifecycle.ViewModel
+
+class ModifyGroupNameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
