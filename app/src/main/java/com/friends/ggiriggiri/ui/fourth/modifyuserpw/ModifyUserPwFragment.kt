dc49176b1755c85dd827a29d5dad04342d0d0a8b
universeYong <boyd0509@gmail.com>
2025-02-18 13:54:18 +0900

    ASY

--- app/src/main/java/com/friends/ggiriggiri/ui/fourth/modifyuserpw/ModifyUserPwFragment.kt
+++ app/src/main/java/com/friends/ggiriggiri/ui/fourth/modifyuserpw/ModifyUserPwFragment.kt
@@ -1,31 +1,70 @@
 package com.friends.ggiriggiri.ui.fourth.modifyuserpw
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.friends.ggiriggiri.R
+import com.friends.ggiriggiri.SocialActivity
+import com.friends.ggiriggiri.databinding.FragmentModifyUserPwBinding
+import com.friends.ggiriggiri.ui.custom.CustomDialog
 
 class ModifyUserPwFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ModifyUserPwFragment()
-    }
-
-    private val viewModel: ModifyUserPwViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    lateinit var fragmentModifyUserPwBinding: FragmentModifyUserPwBinding
+    lateinit var socialActivity: SocialActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_modify_user_pw, container, false)
+
+        fragmentModifyUserPwBinding = FragmentModifyUserPwBinding.inflate(inflater)
+        socialActivity = activity as SocialActivity
+
+        settingToolbar()
+        applyButton()
+
+        return fragmentModifyUserPwBinding.root
+    }
+
+    // Toolbar
+    private fun settingToolbar(){
+        fragmentModifyUserPwBinding.apply {
+            toolbarModifyGroupPw.setTitle("비밀번호 변경")
+            toolbarModifyGroupPw.setNavigationIcon(R.drawable.ic_arrow_back_ios)
+            toolbarModifyGroupPw.setNavigationOnClickListener {
+                socialActivity.supportFragmentManager.popBackStack()
+            }
+        }
+    }
+
+    // button
+    private fun applyButton(){
+        fragmentModifyUserPwBinding.apply {
+            modifyUserPwButton.setOnClickListener {
+                modifyUserPwDialog()
+            }
+        }
+    }
+
+    // 유저 비밀번호 변경 다이얼 로그
+    private fun modifyUserPwDialog(){
+        val dialog = CustomDialog(
+            context = requireContext(),
+            contentText = "비밀번호를 변경하시겠습니까?",
+            icon = R.drawable.ic_logout,
+            positiveText = "예",
+            onPositiveClick = {
+            Toast.makeText(requireContext(), "비밀번호가 변경 되었습니다.", Toast.LENGTH_SHORT).show()
+            },
+            negativeText = "아니오",
+            onNegativeClick = {
+
+            }
+        )
+        dialog.showCustomDialog()
     }
 }
--- app/src/main/java/com/friends/ggiriggiri/ui/fourth/settinggroup/SettingGroupFragment.kt
+++ app/src/main/java/com/friends/ggiriggiri/ui/fourth/settinggroup/SettingGroupFragment.kt
@@ -1,31 +1,151 @@
 package com.friends.ggiriggiri.ui.fourth.settinggroup
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
 import com.friends.ggiriggiri.R
+import com.friends.ggiriggiri.SocialActivity
+import com.friends.ggiriggiri.databinding.FragmentSettingGroupBinding
+import com.friends.ggiriggiri.ui.custom.CustomDialog
+import com.friends.ggiriggiri.ui.fourth.modifygroupname.ModifyGroupNameFragment
+import com.friends.ggiriggiri.ui.fourth.modifygrouppw.ModifyGroupPwFragment
+import com.friends.ggiriggiri.ui.fourth.modifyuserpw.ModifyUserPwFragment
+import com.friends.ggiriggiri.ui.fourth.mypage.MyPageFragment
+import com.friends.ggiriggiri.ui.fourth.mypage.MyPageFragmentName
+import com.google.android.material.transition.MaterialSharedAxis
 
 class SettingGroupFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SettingGroupFragment()
-    }
+    lateinit var fragmentSettingGroupBinding: FragmentSettingGroupBinding
+    lateinit var socialActivity: SocialActivity
 
-    private val viewModel: SettingGroupViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    // 현재 Fragment와 다음 Fragment를 담을 변수(애니메이션 이동 때문에...)
+    var newFragment: Fragment? = null
+    var oldFragment: Fragment? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_setting_group, container, false)
+
+        fragmentSettingGroupBinding = FragmentSettingGroupBinding.inflate(inflater)
+        socialActivity = activity as SocialActivity
+
+        settingToolbar()
+
+        setupGroupOptions()
+
+        return fragmentSettingGroupBinding.root
     }
+
+    // Toolbar
+    private fun settingToolbar(){
+        fragmentSettingGroupBinding.apply {
+            toolbarGroupSetting.setTitle("그룹 설정")
+            toolbarGroupSetting.setNavigationIcon(R.drawable.ic_arrow_back_ios)
+            toolbarGroupSetting.setNavigationOnClickListener {
+                socialActivity.supportFragmentManager.popBackStack()
+            }
+        }
+    }
+
+
+    // 각 텍스트 동작 함수
+    private fun setupGroupOptions() {
+        fragmentSettingGroupBinding.apply {
+            modifyGroupPw.text = "그룹 비밀번호 변경"
+            modifyGroupName.text = "그룹명 변경"
+            modifyGroupExit.text = "그룹 나가기"
+            val groupItemClickListener = View.OnClickListener { view ->
+                when (view.id) {
+                    R.id.modifyGroupPw -> {
+                        // 그룹 비밀번호 변경 동작 구현
+                        replaceFragment(SettingGroupFragmentName.MODIFY_GROUP_PW_FRAGMENT,true,false,null)
+                    }
+
+                    R.id.modifyGroupName -> {
+                        // 그룹명 변경 동작 구현
+                        replaceFragment(SettingGroupFragmentName.MODIFY_GROUP_NAME_FRAGMENT,true,false,null)
+
+                    }
+
+                    R.id.modifyGroupExit -> {
+                        modifyGroupExitDialog()
+                    }
+                }
+            }
+            modifyGroupPw.setOnClickListener(groupItemClickListener)
+            modifyGroupName.setOnClickListener(groupItemClickListener)
+            modifyGroupExit.setOnClickListener(groupItemClickListener)
+        }
+    }
+
+    // 그룹 나가기 다이얼로그
+    private fun modifyGroupExitDialog(){
+        val dialog = CustomDialog(
+            context = requireContext(),
+            contentText = "정말 그룹을 나가시겠습니까?",
+            icon = R.drawable.ic_group_off,
+            positiveText = "예",
+            onPositiveClick = {
+
+            },
+            negativeText = "아니오",
+            onNegativeClick = {
+
+            }
+        )
+        dialog.showCustomDialog()
+    }
+
+
+    // 프래그먼트를 교체하는 함수
+    fun replaceFragment(fragmentName: SettingGroupFragmentName, isAddToBackStack:Boolean, animate:Boolean, dataBundle: Bundle?){
+
+        // 프래그먼트 객체
+        newFragment = when(fragmentName){
+            // 그룹명 변경 화면
+            SettingGroupFragmentName.MODIFY_GROUP_NAME_FRAGMENT -> ModifyGroupNameFragment()
+            // 그룹 비밀번호 변경 화면
+            SettingGroupFragmentName.MODIFY_GROUP_PW_FRAGMENT -> ModifyGroupPwFragment()
+        }
+
+        // bundle 객체가 null이 아니라면
+        if(dataBundle != null){
+            newFragment?.arguments = dataBundle
+        }
+
+        // 프래그먼트 교체
+        socialActivity.supportFragmentManager.commit {
+
+            if(animate) {
+                // 만약 이전 프래그먼트가 있다면
+                if(oldFragment != null){
+                    oldFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+                    oldFragment?.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+                }
+
+                newFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+                newFragment?.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+                newFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+                newFragment?.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+            }
+
+            replace(R.id.fragmentContainerViewSocialMain, newFragment!!)
+            if(isAddToBackStack){
+                addToBackStack(fragmentName.str)
+            }
+        }
+    }
+}
+
+// MYPAGE 프래그먼트
+enum class SettingGroupFragmentName(var number:Int, var str:String){
+    // 그룹명 변경
+    MODIFY_GROUP_NAME_FRAGMENT(1,"ModifyGroupNameFragment"),
+    // 그룹 비밀번호 변경
+    MODIFY_GROUP_PW_FRAGMENT(2,"ModifyGroupPwFragment")
 }
