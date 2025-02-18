7973b02e4678b749ca014eb39e6b3ac534bd3793
universeYong <boyd0509@gmail.com>
2025-02-18 13:54:18 +0900

    ASY

--- app/src/main/java/com/friends/ggiriggiri/ui/fourth/modifygrouppw/ModifyGroupPwFragment.kt
+++ app/src/main/java/com/friends/ggiriggiri/ui/fourth/modifygrouppw/ModifyGroupPwFragment.kt
@@ -1,31 +1,71 @@
 package com.friends.ggiriggiri.ui.fourth.modifygrouppw
 
-import androidx.fragment.app.viewModels
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.friends.ggiriggiri.R
+import com.friends.ggiriggiri.SocialActivity
+import com.friends.ggiriggiri.databinding.FragmentModifyGroupPwBinding
+import com.friends.ggiriggiri.ui.custom.CustomDialog
 
 class ModifyGroupPwFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ModifyGroupPwFragment()
-    }
-
-    private val viewModel: ModifyGroupPwViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    lateinit var fragmentModifyGroupPwBinding: FragmentModifyGroupPwBinding
+    lateinit var socialActivity: SocialActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_modify_group_pw, container, false)
+        fragmentModifyGroupPwBinding = FragmentModifyGroupPwBinding.inflate(inflater)
+        socialActivity = activity as SocialActivity
+
+        settingToolbar()
+
+        applyButton()
+
+        return fragmentModifyGroupPwBinding.root
+    }
+
+    // Toolbar
+    private fun settingToolbar(){
+        fragmentModifyGroupPwBinding.apply {
+            toolbarModifyGroupPw.setTitle("그룹 비밀번호 재설정")
+            toolbarModifyGroupPw.setNavigationIcon(R.drawable.ic_arrow_back_ios)
+            toolbarModifyGroupPw.setNavigationOnClickListener {
+                socialActivity.supportFragmentManager.popBackStack()
+            }
+        }
+    }
+
+    // button
+    private fun applyButton(){
+        fragmentModifyGroupPwBinding.apply {
+            modifyGroupPwButton.setOnClickListener {
+                modifyGroupPwDialog()
+            }
+        }
+    }
+
+    // 그룹 비밀번호 재설정 다이얼로그\
+    private fun modifyGroupPwDialog(){
+        val dialog = CustomDialog(
+            context = requireContext(),
+            contentText = "그룹 비밀번호를 변경하시겠습니까?",
+            icon = R.drawable.ic_logout,
+            positiveText = "예",
+            onPositiveClick = {
+                Toast.makeText(requireContext(), "그룹비밀번호가 변경 되었습니다.", Toast.LENGTH_SHORT).show()
+            },
+            negativeText = "아니오",
+            onNegativeClick = {
+
+            }
+        )
+        dialog.showCustomDialog()
     }
 }
--- app/src/main/java/com/friends/ggiriggiri/ui/fourth/mypage/MyPageBottomSheetFragment.kt
+++ app/src/main/java/com/friends/ggiriggiri/ui/fourth/mypage/MyPageBottomSheetFragment.kt
@@ -2,13 +2,14 @@ package com.friends.ggiriggiri.ui.fourth.mypage
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.friends.ggiriggiri.R
 import com.friends.ggiriggiri.databinding.FragmentMyPageBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -18,26 +19,6 @@ class MyPageBottomSheetFragment : BottomSheetDialogFragment() {
 
     private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        pickImageLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { result ->
-            if (result.resultCode == AppCompatActivity.RESULT_OK) {
-                val imageUri = result.data?.data // 선택된 이미지 URI 가져오기
-
-                if (imageUri != null) {
-                    // 부모 Fragment(MyPageFragment)에 선택한 이미지 전달 및 업데이트 요청
-                    val parentFragment = parentFragment as? MyPageFragment
-                    parentFragment?.updateProfileImage(imageUri)
-                    dismiss() // BottomSheet 닫기
-                }
-            }
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,14 +27,19 @@ class MyPageBottomSheetFragment : BottomSheetDialogFragment() {
 
         fragmentMyPageBottomSheetBinding = FragmentMyPageBottomSheetBinding.inflate(layoutInflater)
 
-        fragmentMyPageBottomSheetBinding.profileModify.setOnClickListener {
-            // 갤러리 Intent 생성 및 실행
-            val intent = Intent(Intent.ACTION_PICK).apply { type = "image/*" }
-            pickImageLauncher.launch(intent)
+        // ActivityResultLauncher 초기화
+        pickImageLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                val imageUri = result.data?.data
+                if (imageUri != null) {
+                    // 선택한 이미지를 프로필 이미지로 설정
+                }
+            }
         }
 
-
-        profileViewClick()
+        profileModifyClick()
 
         return fragmentMyPageBottomSheetBinding.root
     }
@@ -62,18 +48,23 @@ class MyPageBottomSheetFragment : BottomSheetDialogFragment() {
     private fun profileViewClick() {
         fragmentMyPageBottomSheetBinding.apply {
             profileView.setOnClickListener {
-                val parentFragment = parentFragment as? MyPageFragment
-                val imageUri = parentFragment?.getProfileImageUri() // 부모 Fragment에서 URI 가져오기
-
-                if (imageUri != null) {
-                    // FullScreenImageDialogFragment 호출
-                    val dialog = FullScreenImageDialogFragment.newInstance(imageUri)
-                    dialog.show(parentFragmentManager, "FullScreenImageDialog")
-                } else {
-                    Toast.makeText(context, "프로필 이미지를 불러올 수 없습니다.", Toast.LENGTH_SHORT).show()
-                }
+                // 다이얼 로그로 이동
             }
         }
     }
 
+    // 프로필 사진 변경
+    private fun profileModifyClick(){
+        fragmentMyPageBottomSheetBinding.apply {
+            profileModify.setOnClickListener {
+                // 앨범 연결
+                // 갤러리 Intent 생성
+                val intent = Intent(Intent.ACTION_PICK).apply {
+                    type = "image/*"
+                }
+                // ActivityResultLauncher로 Intent 실행
+                pickImageLauncher.launch(intent)
+            }
+        }
+    }
 }
